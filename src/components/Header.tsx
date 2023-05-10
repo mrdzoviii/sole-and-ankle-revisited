6e8323e3dd5e345d1fd0e1011bbdbb553d80f90a
Jovan Danilovic <joco-95@hotmail.com>
2023-05-10 08:48:25 +0200

    feat: fluid desktop navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import styled from "styled-components/macro";
-
+import * as Dialog from "@radix-ui/react-dialog";
 import { useState } from "react";
+import styled from "styled-components/macro";
 import { COLORS, WEIGHTS } from "../constants";
 import Icon from "./Icon";
 import Logo from "./Logo";
@@ -25,8 +25,8 @@ const Header = () => {
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/women">Women fsdfddssdfsd</NavLink>
+          <NavLink href="/kids">Kidssdklsdfkfslsl</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </DesktopNav>
         <MobileActions>
@@ -38,14 +38,33 @@ const Header = () => {
             <Icon id="search" strokeWidth={2} />
             <VisuallyHidden>Search</VisuallyHidden>
           </UnstyledButton>
-          <UnstyledButton>
-            <Icon
-              id="menu"
-              strokeWidth={2}
-              onClick={() => setShowMobileMenu(true)}
-            />
-            <VisuallyHidden>Menu</VisuallyHidden>
-          </UnstyledButton>
+          <Dialog.Root open={showMobileMenu}>
+            <Dialog.DialogTrigger>
+              <UnstyledButton>
+                <Icon
+                  id="menu"
+                  strokeWidth={2}
+                  onClick={() => setShowMobileMenu(true)}
+                />
+                <VisuallyHidden>Menu</VisuallyHidden>
+              </UnstyledButton>
+            </Dialog.DialogTrigger>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Edit profile</Dialog.Title>
+              <Dialog.Description className="DialogDescription" />
+              <Dialog.DialogClose>
+                <UnstyledButton>
+                  <Icon
+                    id="close"
+                    strokeWidth={2}
+                    onClick={() => setShowMobileMenu(false)}
+                  />
+                  <VisuallyHidden>Close</VisuallyHidden>
+                </UnstyledButton>
+              </Dialog.DialogClose>
+            </Dialog.Content>
+          </Dialog.Root>
         </MobileActions>
         <Filler />
       </MainHeader>
@@ -76,7 +95,7 @@ const MainHeader = styled.div`
   border-bottom: 1px solid ${COLORS.gray[300]};
   display: flex;
   align-items: baseline;
-  height: 72px;
+  overflow-x: auto;
 
   ${(p) => p.theme.queries.tabletAndDown} {
     border-top: 4px solid ${COLORS.gray[900]};
@@ -92,7 +111,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
   ${(p) => p.theme.queries.tabletAndDown} {
@@ -122,6 +141,7 @@ const NavLink = styled.a`
   text-decoration: none;
   color: ${COLORS.gray[900]};
   font-weight: ${WEIGHTS.medium};
+  white-space: nowrap;
 
   &:first-of-type {
     color: ${COLORS.secondary};
